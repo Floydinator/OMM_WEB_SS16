ac5b4972d0bd16ebe99e1e59d176b2e805f3810c
Erich <erich.keller.m@gmail.com>
2016-04-20 14:30:02 +0200

    head ausgelagert und bootstrap einbindung ergänzt um javascript und responsive webdesign navigation ausgelagert admin.php strukturiert = so sollte der code überall aussehen

---
+++ includes/head.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+
+
+<!-- Start Bootstrap Einbindung -->
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Ende Bootstrap Einbindung -->
+
+
+<!-- Start Custom CSS -->
+
+    <!--<link href="css/landing-page.css" rel="stylesheet"> -->
+    <link type="text/css" rel="stylesheet" href="../css/admin.css">
+
+<!-- Ende Custom CSS -->
