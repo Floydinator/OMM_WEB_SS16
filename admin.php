a6170e49baad9f9f54605b8b2d99efd28a420ec4
Erich <erich.keller.m@gmail.com>
2016-05-03 10:33:37 +0200

    Design läuft wieder

--- admin.php
+++ admin.php
@@ -5,10 +5,31 @@
     <head>
 
         <title>Administration</title>
-        <?php include ('includes/head.php'); ?>
+
+        <meta charset="utf-8"/>
+
+
+        <!-- Start Bootstrap Einbindung -->
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> <!-- Die Fehlermeldung kann ignoriert werden -->
+        <script src="/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Ende Bootstrap Einbindung -->
+
+
+        <!-- Start Custom CSS -->
+
+        <link href="css/landing-page.css" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/admin.css">
+
+        <!-- Ende Custom CSS -->
+
+
         <?php include ("includes/verbindung.php"); ?>
         <?php include ("includes/navigation.php"); ?>
-        
+
     </head>
         
     <body>
