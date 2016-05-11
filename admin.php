36f49815578c1835c899d1dad9d9363b65a381f9
Floydinator <flori.schoenberger@t-online.de>
2016-05-11 16:00:25 +0200

    zusammengeführte und aktualisierte dateien, funktioniert alles

--- admin.php
+++ admin.php
@@ -19,12 +19,38 @@
 
     <!-- Start Include Dateien -->
     <?php include ("includes/verbindung.php"); ?>
-    <?php include ("includes/navigation.php"); ?>
     <?php include ("includes/reg.php"); ?>
     <!-- Ende Include Dateien -->
 </head>
 
 <body>
+
+<!-- Start Navigation -->
+<nav class="navbar navbar-default topnav" role="navigation">
+    <div class="container topnav">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand topnav" id="brand">TeachBox</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="login.html">Logout</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+<!-- Ende Navigation -->
 
 <!-- Start Dozenten aus DB abfragen -->
 
